1c8401ec3d335cca6828d1c420c09fcaaa37145f
Prabin <manandharprabin819@gmail.com>
2024-07-23 00:25:20 +0545

    10. => Image and ImageBackground component

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,11 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, ImageBackground } from "react-native";
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Text>
-        <Text style={{ color: "white" }}>Hello </Text>World
-      </Text>
+      {/* <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      <Image
+        source={{ uri: "https://picsum.photos/300" }}
+        style={{ width: 300, height: 300 }}  //specifying height and width for netwoek inages is mandatory for netowrk images
+      /> */}
+      <ImageBackground source={logoImg} style={{ flex: 1 }}>
+        <Text>image text here</Text>
+      </ImageBackground>
     </View>
   );
 }
