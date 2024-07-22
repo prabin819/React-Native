5861c8c53e0293dde09f4dcfc633c81c9821718a
Prabin <manandharprabin819@gmail.com>
2024-07-22 16:56:48 +0545

    9. => Text component

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,22 +1,11 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum" }}>
-      <View
-        style={{
-          backgroundColor: "lightblue",
-          height: 200,
-          width: 200,
-        }}
-      ></View>
-      <View
-        style={{
-          backgroundColor: "lightgreen",
-          height: 200,
-          width: 200,
-        }}
-      ></View>
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <Text>
+        <Text style={{ color: "white" }}>Hello </Text>World
+      </Text>
     </View>
   );
 }
