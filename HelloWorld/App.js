611521e990ae6b58361c0f495d96a23ae2e9ea6a
Prabin <manandharprabin819@gmail.com>
2024-07-22 16:48:37 +0545

    8. => View component

---
+++ HelloWorld/App.js
@@ -0,0 +1,22 @@
+import { View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "plum" }}>
+      <View
+        style={{
+          backgroundColor: "lightblue",
+          height: 200,
+          width: 200,
+        }}
+      ></View>
+      <View
+        style={{
+          backgroundColor: "lightgreen",
+          height: 200,
+          width: 200,
+        }}
+      ></View>
+    </View>
+  );
+}
