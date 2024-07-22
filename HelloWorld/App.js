0c7786fed0ac4f8e587675611be2c0f440d04f30
Prabin <manandharprabin819@gmail.com>
2024-07-23 03:33:09 +0545

    21. => multiple styles

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -3,7 +3,13 @@ import { View, Text, StyleSheet } from "react-native";
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Stylesheet api</Text>
+      <View style={[styles.lightblueBg, styles.box]}>
+        {/* last style in array takes precedence */}
+        <Text>Lightblue box</Text>
+      </View>
+      <View style={[styles.lightgreenBg, styles.box]}>
+        <Text>Lightgreen box</Text>
+      </View>
     </View>
   );
 };
@@ -16,8 +22,16 @@ export const styles = StyleSheet.create({
     backgroundColor: "plum",
     padding: 60,
   },
-  title: {
-    color: "blue",
+  box: {
+    width: 100,
+    height: 100,
+    padding: 10,
+  },
+  lightblueBg: {
+    backgroundColor: "lightblue",
+  },
+  lightgreenBg: {
+    backgroundColor: "lightgreen",
   },
 });
 
