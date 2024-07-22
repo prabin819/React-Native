befcb6ad51c1157f448c6c2a303925885a209305
Prabin <manandharprabin819@gmail.com>
2024-07-23 02:25:09 +0545

    15. => Status bar component

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -8,6 +8,7 @@ import {
   Button,
   Pressable,
   Modal,
+  StatusBar,
 } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 
@@ -15,27 +16,14 @@ export default function App() {
   const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Button
-        title="press"
-        onPress={() => setIsModalVisible(true)}
-        color="midnightblue"
+      <StatusBar
+        backgroundColor="lightgreen" //this porp is specific to android
+        //cant change bgcolor in iphone
+        barStyle="light-content" //textcolor = default is default(black on iphone and white on android)
+        //dark-content = black text on both platforms
+        //light-content = white text on both platforms
+        hidden
       />
-      <Modal
-        visible={isModalVisible}
-        onRequestClose={() => setIsModalVisible(false)}
-        //triggered when user presses back button on android or dismisses the modal with a gesture on ios
-        animationType="slide" //none by default, others are slide and fade
-        presentationStyle="pageSheet" //fullscreen by default, others are formSheet and pageSheet//and haha this prop works only on ios and not on andriod
-      >
-        <View style={{ flex: 1, backgroundColor: "lightgreen", padding: 60 }}>
-          <Text>Modal content</Text>
-          <Button
-            title="close modal"
-            color="midnightblue"
-            onPress={() => setIsModalVisible(false)}
-          />
-        </View>
-      </Modal>
     </View>
   );
 }
