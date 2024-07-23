65aeb418fb35e8071f65546a69fd4707752cd60d
Prabin <manandharprabin819@gmail.com>
2024-07-23 15:45:00 +0545

    31. => alignSelf

--- LayoutFlexbox/App.js
+++ LayoutFlexbox/App.js
@@ -5,13 +5,21 @@ export default function App() {
   return (
     //<View style={{ backgroundColor: "plum", flex: 1 }}></View>
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
-      <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
-      <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
-      {/* <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
-      <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "#8e9b00", alignSelf: "flex-start" }}>
+        Box 1
+      </Box>
+      <Box style={{ backgroundColor: "#b65d1f", alignSelf: "flex-end" }}>
+        Box 2
+      </Box>
+      <Box style={{ backgroundColor: "#1c4c56", alignSelf: "center" }}>
+        Box 3
+      </Box>
+      <Box style={{ backgroundColor: "#ab9156", alignSelf: "stretch" }}>
+        Box 4
+      </Box>
+      <Box style={{ backgroundColor: "#6b0803", alignSelf: "auto" }}>Box 5</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
-      <Box style={{ backgroundColor: "#b95f21" }}>Box 7</Box> */}
+      <Box style={{ backgroundColor: "#b95f21" }}>Box 7</Box>
     </View>
   );
 }
@@ -22,6 +30,5 @@ const styles = StyleSheet.create({
     borderWidth: 6,
     borderColor: "red",
     flex: 1,
-    alignItems: "center",
   },
 });
