0a5ee664c36635ccce4ce559ed792faa4f0e9ae8
Prabin <manandharprabin819@gmail.com>
2024-07-23 14:45:12 +0545

    27. => Flex

--- LayoutFlexbox/App.js
+++ LayoutFlexbox/App.js
@@ -3,9 +3,10 @@ import Box from "./components/Box";
 
 export default function App() {
   return (
+    //<View style={{ backgroundColor: "plum", flex: 1 }}></View>
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
-      <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "#8e9b00", flex: 1 }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#b65d1f", flex: 3 }}>Box 2</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
       <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
       <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
@@ -20,5 +21,6 @@ const styles = StyleSheet.create({
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
+    flex: 1,
   },
 });
