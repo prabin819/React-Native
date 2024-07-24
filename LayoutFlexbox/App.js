71b891f09d615f38b73cf2f0e2d59a3d9d81a32d
Prabin <manandharprabin819@gmail.com>
2024-07-24 12:13:03 +0545

    38. => Relative and Absolute layout

--- LayoutFlexbox/App.js
+++ LayoutFlexbox/App.js
@@ -5,13 +5,22 @@ export default function App() {
   return (
     //<View style={{ backgroundColor: "plum", flex: 1 }}></View>
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#8e9b00", top: 75, left: 75 }}>Box 1</Box>
       <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
-      <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
-      <Box style={{ backgroundColor: "#6b0803", flexGrow: 1 }}>Box 5</Box>
-      <Box style={{ backgroundColor: "#1c4c56", flexGrow: 2 }}>Box 6</Box>
-      <Box style={{ backgroundColor: "#b95f21", flexGrow: 2 }}>Box 7</Box>
+      <Box
+        style={{
+          backgroundColor: "#ab9156",
+          position: "absolute",
+          top: 100,
+          left: 100,
+        }}
+      >
+        Box 4
+      </Box>
+      <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
+      <Box style={{ backgroundColor: "#b95f21" }}>Box 7</Box>
     </View>
   );
 }
--- LayoutFlexbox/components/Box.js
+++ LayoutFlexbox/components/Box.js
@@ -14,6 +14,8 @@ const styles = StyleSheet.create({
   box: {
     backgroundColor: "white",
     padding: 20,
+    width: 100,
+    height: 100,
   },
   text: {
     fontSize: 24,
