9f1c706d6a41951e3f9929f7d32ea7ef6f7b3392
Prabin <manandharprabin819@gmail.com>
2024-07-24 11:58:42 +0545

    36. => flexGrow

--- LayoutFlexbox/App.js
+++ LayoutFlexbox/App.js
@@ -7,15 +7,11 @@ export default function App() {
     <View style={styles.container}>
       <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
       <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
-      <Box style={{ backgroundColor: "#1c4c56", flexBasis: 140, flex: 1 }}>
-        Box 3
-      </Box>
-      <Box style={{ backgroundColor: "#ab9156", height: 140, flex: 1 }}>
-        Box 4
-      </Box>
-      <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
-      <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
-      <Box style={{ backgroundColor: "#b95f21" }}>Box 7</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "#6b0803", flexGrow: 1 }}>Box 5</Box>
+      <Box style={{ backgroundColor: "#1c4c56", flexGrow: 2 }}>Box 6</Box>
+      <Box style={{ backgroundColor: "#b95f21", flexGrow: 2 }}>Box 7</Box>
     </View>
   );
 }
