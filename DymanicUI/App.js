c4729e692eb1d23a174ad72d2709448f5453b7b9
Prabin <manandharprabin819@gmail.com>
2024-07-24 15:04:36 +0545

    40. => Dimensions API Drawback and Solution

--- DymanicUI/App.js
+++ DymanicUI/App.js
@@ -1,17 +1,43 @@
+import { useEffect, useState } from "react";
 import { StyleSheet, View, Text, Dimensions } from "react-native";
 
 export default function App() {
+  const [dimensions, setDimensions] = useState({
+    window: Dimensions.get("window"),
+  });
+
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener("change", ({ window }) => {
+      setDimensions({ window });
+    });
+    return () => subscription?.remove();
+  });
+
+  const { window } = dimensions;
+  // console.log(window); // {"fontScale": 1.100000023841858, "height": 859.8095238095239, "scale": 2.625, "width": 411.42857142857144}
+  const windowWidth = window.width;
+  const windowHeight = window.height;
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text>Welcome</Text>
+      <View
+        style={[
+          styles.box,
+          {
+            width: windowWidth > 500 ? "70%" : "90%",
+            height: windowHeight > 600 ? "60%" : "90%",
+          },
+        ]}
+      >
+        <Text style={{ fontSize: windowWidth > 500 ? 50 : 24 }}>Welcome</Text>
       </View>
     </View>
   );
 }
 
-const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
+// const windowWidth = Dimensions.get("window").width;
+// const windowHeight = Dimensions.get("window").height;
+
+// console.log({ windowWidth, windowHeight });
 
 const styles = StyleSheet.create({
   container: {
@@ -21,13 +47,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   box: {
-    width: windowWidth > 500 ? "70%" : "90%",
-    height: windowHeight > 600 ? "60%" : "90%",
+    // width: windowWidth > 500 ? "70%" : "90%",
+    // height: windowHeight > 600 ? "60%" : "90%",
     backgroundColor: "lightblue",
     alignItems: "center",
     justifyContent: "center",
   },
-  text: {
-    fontSize: windowWidth > 500 ? 50 : 24,
-  },
+  // text: {
+  //   fontSize: windowWidth > 500 ? 50 : 24,
+  // },
 });
