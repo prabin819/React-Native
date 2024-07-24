b11225c68443e2961db75fdc65cb1999d3121430
Prabin <manandharprabin819@gmail.com>
2024-07-24 14:33:37 +0545

    40. => Dimensions API

--- DymanicUI/App.js
+++ DymanicUI/App.js
@@ -1,8 +1,17 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text, Dimensions } from "react-native";
 
 export default function App() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text>Welcome</Text>
+      </View>
+    </View>
+  );
 }
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
   container: {
@@ -10,5 +19,15 @@ const styles = StyleSheet.create({
     backgroundColor: "plum",
     alignItems: "center",
     justifyContent: "center",
+  },
+  box: {
+    width: windowWidth > 500 ? "70%" : "90%",
+    height: windowHeight > 600 ? "60%" : "90%",
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: windowWidth > 500 ? 50 : 24,
   },
 });
