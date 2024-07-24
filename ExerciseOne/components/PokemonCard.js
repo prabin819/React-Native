68b2ce6b9c6845e9b3b48700b3a5cf739ccb243d
Prabin <manandharprabin819@gmail.com>
2024-07-24 23:40:37 +0545

    46. => Pokemon Card (2/6)

--- ExerciseOne/components/PokemonCard.js
+++ ExerciseOne/components/PokemonCard.js
@@ -1,12 +1,33 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
 import React from "react";
 
 const PokemonCard = () => {
   return (
-    <View>
+    <View style={styles.card}>
       <Text>PokemonCard</Text>
     </View>
   );
 };
 
 export default PokemonCard;
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+    borderRadius: 16,
+    borderWidth: 2,
+    padding: 16,
+    margin: 16,
+    ...Platform.select({
+      ios: {
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: "#333",
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+});
