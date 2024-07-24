a3a09efb89f304350566885ee0e42e5184f8ca99
Prabin <manandharprabin819@gmail.com>
2024-07-24 23:31:53 +0545

    45. => Pokemon Card (1/6)

---
+++ ExerciseOne/components/PokemonCard.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const PokemonCard = () => {
+  return (
+    <View>
+      <Text>PokemonCard</Text>
+    </View>
+  );
+};
+
+export default PokemonCard;
