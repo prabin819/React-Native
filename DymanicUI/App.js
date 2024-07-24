4a2d67fd21b559f763eb5c87f7bd556fa1218e79
Prabin <manandharprabin819@gmail.com>
2024-07-24 15:58:08 +0545

    43. => Safe area view (42. => useWindowDimensions chutyo jasto cha)

--- DymanicUI/App.js
+++ DymanicUI/App.js
@@ -1,59 +1,63 @@
-import { useEffect, useState } from "react";
-import { StyleSheet, View, Text, Dimensions } from "react-native";
+// import { StyleSheet, View, Text } from "react-native";
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <View style={[styles.box]}>
+//         <Text style={styles.text}>Welcome!</Text>
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "plum",
+//   },
+//   box: {
+//     padding: 20,
+//   },
+//   text: {
+//     fontSize: 24,
+//     fontWeight: "bold",
+//     textAlign: "center",
+//   },
+// });
+//in ios we dont see welcome cuz its hiddem behind the notch
+//we can use padding:60 to the container but this is not the correct way
+//another problem is that companies may release a new device with notch at a different place
+//solution is to find a safe area
+
+import { StyleSheet, View, Text, SafeAreaView } from "react-native";
 
 export default function App() {
-  const [dimensions, setDimensions] = useState({
-    window: Dimensions.get("window"),
-  });
-
-  useEffect(() => {
-    const subscription = Dimensions.addEventListener("change", ({ window }) => {
-      setDimensions({ window });
-    });
-    return () => subscription?.remove();
-  });
-
-  const { window } = dimensions;
-  // console.log(window); // {"fontScale": 1.100000023841858, "height": 859.8095238095239, "scale": 2.625, "width": 411.42857142857144}
-  const windowWidth = window.width;
-  const windowHeight = window.height;
   return (
-    <View style={styles.container}>
-      <View
-        style={[
-          styles.box,
-          {
-            width: windowWidth > 500 ? "70%" : "90%",
-            height: windowHeight > 600 ? "60%" : "90%",
-          },
-        ]}
-      >
-        <Text style={{ fontSize: windowWidth > 500 ? 50 : 24 }}>Welcome</Text>
+    <SafeAreaView style={styles.safeContainer}>
+      <View style={styles.container}>
+        <View style={[styles.box]}>
+          <Text style={styles.text}>Welcome!</Text>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
-// const windowWidth = Dimensions.get("window").width;
-// const windowHeight = Dimensions.get("window").height;
-
-// console.log({ windowWidth, windowHeight });
-
 const styles = StyleSheet.create({
+  safeContainer: {
+    flex: 1,
+    backgroundColor: "plum",
+  },
   container: {
     flex: 1,
     backgroundColor: "plum",
-    alignItems: "center",
-    justifyContent: "center",
   },
   box: {
-    // width: windowWidth > 500 ? "70%" : "90%",
-    // height: windowHeight > 600 ? "60%" : "90%",
-    backgroundColor: "lightblue",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 20,
   },
-  // text: {
-  //   fontSize: windowWidth > 500 ? 50 : 24,
-  // },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
 });
