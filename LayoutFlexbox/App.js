76d22b5e2ea6885ed4e51555022025bce83bcdae
Prabin <manandharprabin819@gmail.com>
2024-07-24 10:22:29 +0545

    35. => flexBasis

--- LayoutFlexbox/App.js
+++ LayoutFlexbox/App.js
@@ -7,8 +7,12 @@ export default function App() {
     <View style={styles.container}>
       <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
       <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
-      <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
-      <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "#1c4c56", flexBasis: 140, flex: 1 }}>
+        Box 3
+      </Box>
+      <Box style={{ backgroundColor: "#ab9156", height: 140, flex: 1 }}>
+        Box 4
+      </Box>
       <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
       <Box style={{ backgroundColor: "#b95f21" }}>Box 7</Box>
@@ -21,11 +25,11 @@ const styles = StyleSheet.create({
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
-    // flex: 1,
-    height: 300,
-    flexWrap: "wrap",
+    flex: 1,
+    // height: 300,
+    // flexWrap: "wrap",
     // rowGap: 20,
     // columnGap: 30,
-    gap: 10,
+    //gap: 10,
   },
 });
