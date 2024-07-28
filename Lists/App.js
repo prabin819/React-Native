a3ea6cc9d1255b3abc9aadbf129d9283a3e740ef
Prabin <manandharprabin819@gmail.com>
2024-07-28 14:48:06 +0545

    51. => Lists (map is not the preferred technique)

---
+++ Lists/App.js
@@ -0,0 +1,48 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  SafeAreaView,
+  StatusBar,
+} from "react-native";
+import pokemonList from "./data.json";
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        {pokemonList.map(
+          (
+            pokemon //not the preferred technique
+          ) => (
+            <View style={styles.card} key={pokemon.id}>
+              <Text style={styles.cardText}>{pokemon.type}</Text>
+              <Text style={styles.cardText}>{pokemon.name}</Text>
+            </View>
+          )
+        )}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    paddingHorizontal: 16,
+  },
+  card: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    marginBottom: 16,
+  },
+  cardText: {
+    fontSize: 30,
+  },
+});
